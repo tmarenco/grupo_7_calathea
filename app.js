abfb406d780f8e9a23a77680e01ae367de85ca9f
tmarenco <tmarencocastello@gmail.com>
2021-01-02 12:59:11 -0300

    Agregué el register y la base del carrito

--- app.js
+++ app.js
@@ -18,3 +18,12 @@ app.get ("/login", (req, res) => {
 });
 
 
+app.get ("/register", (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/register.html"));
+});
+
+app.get ("/carrito", (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/carrito.html"));
+});
+
+
