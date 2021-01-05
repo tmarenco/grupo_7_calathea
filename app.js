405c6f8a4386b0e5b8af64a4942e329bd9d02ecf
iruleicach <iru.leicach@gmail.com>
2021-01-05 02:52:43 -0300

    Modifiqué estructura html y css del home

--- app.js
+++ app.js
@@ -6,24 +6,27 @@ const publicPath = path.resolve (__dirname, "./public");
 
 app.use (express.static(publicPath));
 
+
 app.listen (3000, () => console.log ("Servidor corriendo en el puerto 3000"));
 
+
+
 app.get ("/", (req, res) => {
-    res.sendFile (path.resolve (__dirname, "./views/home.html"));
+    res.sendFile (path.resolve (__dirname, "./views/index.html"));
 });
 
-
-app.get ("/login", (req, res) => {
-    res.sendFile (path.resolve (__dirname, "./views/login.html"));
+app.get ("/productDetail", (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/productDetail.html"));
 });
 
+app.get ("/productCart", (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/productCart.html"));
+});
 
 app.get ("/register", (req, res) => {
     res.sendFile (path.resolve (__dirname, "./views/register.html"));
 });
 
-app.get ("/carrito", (req, res) => {
-    res.sendFile (path.resolve (__dirname, "./views/carrito.html"));
+app.get ("/login", (req, res) => {
+    res.sendFile (path.resolve (__dirname, "./views/login.html"));
 });
-
-
