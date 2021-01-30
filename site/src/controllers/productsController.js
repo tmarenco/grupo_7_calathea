cadb36c4bcf5201eca97e87b255e4900e8f73b12
tmarenco <tmarencocastello@gmail.com>
2021-01-30 20:37:01 -0300

    Arreglé el home y le puse vistas dinámicas a los productos

--- site/src/controllers/productsController.js
+++ site/src/controllers/productsController.js
@@ -1,15 +1,17 @@
 const path = require ("path");
+const fs = require ("fs");
+
+//Esto vamos a tener que transformarlo en una función más adelante, en principio lo hago así//
+let productos = fs.readFileSync(path.join (__dirname, "../data/products.json"), "utf-8");
+productos = JSON.parse(productos);
 
 module.exports = {
 
-
+//Creé otro archivo ejs que se llama "product2.ejs" solo para hacer pruebas y no sobreescribir el "products.ejs". La ruta products está justamente asociada al products2.ejs//
     Products: (req, res) => {
-        res.render (path.join (__dirname, "../views/products/products.ejs"))
+        res.render (path.join (__dirname, "../views/products/products2.ejs"), {productos})
     },
 
-    /*Products: (req, res) => {
-        res.render (path.join (__dirname, "../views/products/products.ejs"))
-    },*/
 
     Detail: (req, res) => {
         res.render (path.join (__dirname, "../views/products/productDetail.ejs"))
