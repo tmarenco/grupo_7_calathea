e3333aa1c9e5f56bf1d0122b56f620503ceeeccd
tmarenco <tmarencocastello@gmail.com>
2021-08-02 23:46:17 -0300

    Modificaciones para la web en heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,36 @@
+// Conexión a base de datos local
+// module.exports = {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "calathea",
+//     "host": "localhost",
+//     "dialect": "mysql",
+//     "port" : 3306
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+// Conexión a base de datos remota
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "calathea",
-    "host": "localhost",
+    "username": "JQRuhWlEVL",
+    "password": "17fDRr99Qa",
+    "database": "JQRuhWlEVL",
+    "host": "remotemysql.com",
     "dialect": "mysql",
     "port" : 3306
   },
